b12e90cc7adaf1598438ae0fc5af0873dbade6cb
muki26 <56950610+muki26@users.noreply.github.com>
2020-11-13 23:00:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+var fname ;
+var button = document.getElementById('button');
+button.addEventListener('click', ()=>{
+    var muki;
+   
+   fname = document.getElementById('name').value;
+    console.log('your output is' + fname);
+    muki = fname.length;
+    if(fname == ""){
+        alert("Peru Adida Venna..!");
+    }
+    else if(fname <= 1000000000000000000000000000000000000000000000000000000){
+        alert("Un Peru Adida Tousser uhh...!");
+    }
+    else if(muki == 1 )
+    alert("IAS Velaya Paakura ... ? Un Pera Adida");
+    else if(muki == 2 )
+    alert("Adinguu.... Pambara Katta Mandaya");
+    else if(muki == 3)
+    alert("Nambura Mathiri Ilayae...! ");
+    else {
+        location = "page1.html";
+    }
+
+    
+});
+
+
